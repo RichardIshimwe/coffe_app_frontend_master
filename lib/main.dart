b8e37ad41e49ce17830b3abe558ba94aadca77e6
RichardIshimwe <ishimwerichard26@gmail.com>
2024-07-28 03:06:24 +0200

    introduction to widgets(both statefull and stateless)

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,42 @@ void main() {
   runApp(const MyApp());
 }
 
+class Greet extends StatefulWidget {
+  const Greet({super.key});
+
+  @override
+  State<Greet> createState() => _GreetState();
+}
+
+class _GreetState extends State<Greet> {
+  String name = "";
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("I am learning $name for the first time"),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            onChanged: (value) => setState(() {
+              name = value;
+            }),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class HelloWorld extends StatelessWidget {
+  const HelloWorld({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("this is a text widget");
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -43,20 +79,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
+      body: Greet(),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
