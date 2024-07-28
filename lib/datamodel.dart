7161cfdd142718c6f1beaaab597ae9409776cb34
RichardIshimwe <ishimwerichard26@gmail.com>
2024-07-28 23:21:09 +0200

    enhancement : data modeling]

---
+++ lib/datamodel.dart
@@ -0,0 +1,22 @@
+class Product {
+  int id;
+  String name;
+  double price;
+  String image;
+
+  Product({required this.id,required this.name, required this.price, required this.image});
+}
+
+class Category {
+  String name;
+  List<Product> products;
+
+  Category({required this.name, required this.products});
+}
+
+class ItemInCart {
+  Product product;
+  int quantity;
+
+  ItemInCart({required this.product, required this.quantity});
+}
